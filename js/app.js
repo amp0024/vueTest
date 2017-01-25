3cb5c120c756472efab40df13166c92347a99e04
apoulsen <amp0024@gmail.com>
2017-01-24 17:52:12 -0700

    basic v-bind directive

--- js/app.js
+++ js/app.js
@@ -4,25 +4,7 @@ new Vue({
 	el: '#app',
 	// Accesable in our app - can be outputed into html file.
 	data: {
-		firstName: 'Adam',
-		lastName: 'Poulsen'
-	},
-	methods: {
-		// key        //value
-		// getFullName: function(){
-		// 	return this.firstName + ' ' + this.lastName;
-		// }
-
-		// using parameters - these values are asigned in the template file
-		// getFullName: function(first, last){
-		// 	return first + ' ' + last;
-		// }
-
-		getFullName: function(first, last){
-			return {
-				name: first + ' ' + last
-			};
-		}		
+		blogUrl: 'http://codingexplained.com'
 	}
 });
 
