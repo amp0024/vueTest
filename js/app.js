2a964629b655f6f4d61e445e5aad912e178d9fbc
apoulsen <amp0024@gmail.com>
2017-01-24 17:16:52 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// setting up vue constructor
+new Vue({
+	// controlling html ID element
+	el: '#app',
+	// Accesable in our app - can be outputed into html file.
+	data: {
+		message: 'Hello World!'
+	}
+});
