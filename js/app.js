df5e905ed178cbf14735e5652a53b2ae1c6a27c6
apoulsen <amp0024@gmail.com>
2017-01-24 17:42:34 -0700

    basic method examples

--- js/app.js
+++ js/app.js
@@ -4,8 +4,25 @@ new Vue({
 	el: '#app',
 	// Accesable in our app - can be outputed into html file.
 	data: {
-		age: 27,
-		name: 'Adam Poulsen'
+		firstName: 'Adam',
+		lastName: 'Poulsen'
+	},
+	methods: {
+		// key        //value
+		// getFullName: function(){
+		// 	return this.firstName + ' ' + this.lastName;
+		// }
+
+		// using parameters - these values are asigned in the template file
+		// getFullName: function(first, last){
+		// 	return first + ' ' + last;
+		// }
+
+		getFullName: function(first, last){
+			return {
+				name: first + ' ' + last
+			};
+		}		
 	}
 });
 
